05b8624b6ce3903bf42ef4a62ddae47969fa2aec
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-02-16 16:25:51 +0000

    typing effect added on span in js

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-var navLinksEl = $(".nav-links");
-var toggleBtn = $(".toggle-btn");
-var ulEl = $(".ul-toggle");
-var ulDivEl = $(".ul-container");
-var logoEl = $(".logo");
+const navLinksEl = $(".nav-links");
+const toggleBtn = $(".toggle-btn");
+const ulEl = $(".ul-toggle");
+const ulDivEl = $(".ul-container");
+const logoEl = $(".logo");
 
+// button nav toggle
 toggleBtn.on("click", function () {
   $("i", $(this)).toggleClass(
     "fa-sharp fa-solid fa-circle-chevron-down fa-solid fa-circle-chevron-up"
   );
-  // if (ulDivEl.hasClass("hidden")) {
-  //   ulDivEl.removeClass("hidden");
-  // } else {
-  //   ulDivEl.addClass("hidden");
-  // }
   ulDivEl.toggle("slow");
+});
+
+// word typing effect
+const typedText = document.querySelector(".text");
+const wordsArray = ["Hello", "Labas", "Привeт"];
+let i = 0;
+let j = 0;
+
+function type() {
+  if (j < wordsArray[i].length) {
+    typedText.textContent += wordsArray[i].charAt(j);
+    console.log(j);
+    j++;
+    setTimeout(type, 300);
+  } else {
+    setTimeout(erase, 1000);
+  }
+}
+function erase() {
+  if (j > 0) {
+    typedText.textContent = wordsArray[i].substring(0, j - 1);
+    j--;
+    setTimeout(erase, 100);
+  } else {
+    i++;
+    if (i >= wordsArray.length) i = 0;
+    setTimeout(type, 300);
+  }
+}
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, 500);
 });
